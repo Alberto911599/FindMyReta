c57914d4e452915a7bc093dd1c012ac2ae20dd95
unknown <beto.911599@gmail.com>
2019-11-20 02:09:55 -0600

    Missing models

---
+++ models.js
@@ -0,0 +1,57 @@
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let placeSchema = mongoose.Schema({
+    location : {type : String},
+    typeOfSports : {type : String},
+    cost : {type : String},
+    image : {type : String},
+    nowPlaying : {type : String}
+});
+
+let Place = mongoose.model('Reta', placeSchema);
+
+let PlaceList = {
+    get : function(){
+        console.log("Place get");
+		return Place.find()
+				.then( places => {
+					return places;
+				})
+				.catch( error => {
+					throw Error( error );
+				});
+	},
+    post : function(newPlace){
+        console.log("Place Post");
+        return Place.create(newPlace)
+                .then( place => {
+                    return place;
+                })
+                .catch( err=> {
+                    throw Error(err);   
+                });
+    },
+    put : function(filer, updatedInfo){
+        console.log("Place Put");
+        return Place.updateOne(filer, updatedInfo)
+                .then( place => {
+                    return place;
+                })
+                .catch( err=> {
+                    throw Error(err);   
+                });
+    },
+    delete :  function(filter){
+        console.log("Place Delete");
+        return Place.deleteOne(filter)
+            .then( place => {
+                return place;
+            })
+            .catch( err=> {
+                throw Error(err);   
+            });
+    }
+}
+
+module.exports = { PlaceList };
