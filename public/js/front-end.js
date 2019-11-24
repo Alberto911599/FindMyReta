ca544a43834d28a4e18daae9377e684875eae89c
unknown <beto.911599@gmail.com>
2019-11-24 01:12:23 -0600

    autocomplete

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -40,7 +40,7 @@ function initAutocomplete() {
     // Create the autocomplete object, restricting the search predictions to
     // geographical location types.
     autocomplete = new google.maps.places.Autocomplete(
-        $("#locationInput"), {types: ['geocode']});
+        document.getElementById("locationInput"), {types: ['geocode']});
   
     // Avoid paying for data that you don't need by restricting the set of
     // place fields that are returned to just the address components.
